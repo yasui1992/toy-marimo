2225f0ce8c7dc2687c2d89d555194a8813c590a6
yasui1992 <yasui1992github@gmail.com>
2025-06-09 23:06:09 +0900

    Add interactive_pandas_and_altair.py

---
+++ mnt/notebooks/interactive_pandas_and_altair.py
@@ -0,0 +1,52 @@
+import marimo
+
+__generated_with = "0.13.15"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _():
+    import pandas as pd
+    import altair as alt
+    return alt, pd
+
+
+@app.cell
+def _(pd):
+    df = pd.read_json("https://github.com/vega/vega-datasets/raw/main/data/penguins.json")
+    df
+    return (df,)
+
+
+@app.cell
+def _(df, mo):
+    mo.ui.dataframe(df)
+    return
+
+
+@app.cell
+def _(alt, df, mo):
+    chart = alt.Chart(df).mark_point().encode(
+        x="Beak Length (mm)",
+        y="Beak Depth (mm)",
+        color="Species",
+    )
+
+    chart = mo.ui.altair_chart(chart)
+    return (chart,)
+
+
+@app.cell
+def _(chart, mo):
+    mo.vstack([chart, chart.value.head()])
+    return
+
+
+if __name__ == "__main__":
+    app.run()

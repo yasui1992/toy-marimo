dce7f0bc70fd009a396c0293ac6b87d25eb2dcea
yasui1992 <162020312+yasui1992@users.noreply.github.com>
2025-06-09 23:28:42 +0900

    Feature/use marimo UI (#5)
    * Add interactive_pandas_and_altair.py
    
    * Clean up python package deps
    
    * Small Fix

---
+++ mnt/notebooks/interactive_pandas_and_altair.py
@@ -0,0 +1,48 @@
+import marimo
+
+__generated_with = "0.13.15"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    import pandas as pd
+    import altair as alt
+    return alt, mo, pd
+
+
+@app.cell
+def _(pd):
+    df = pd.read_json("https://github.com/vega/vega-datasets/raw/main/data/penguins.json")
+    df
+    return (df,)
+
+
+@app.cell
+def _(df, mo):
+    mo.ui.dataframe(df)
+    return
+
+
+@app.cell
+def _(alt, df, mo):
+    chart = alt.Chart(df).mark_point().encode(
+        x="Beak Length (mm)",
+        y="Beak Depth (mm)",
+        color="Species",
+    )
+
+    chart = mo.ui.altair_chart(chart)
+    return (chart,)
+
+
+@app.cell
+def _(chart, mo):
+    mo.vstack([chart, chart.value])
+    return
+
+
+if __name__ == "__main__":
+    app.run()

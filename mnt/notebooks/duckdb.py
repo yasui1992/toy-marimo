22edb8cea2edfd0e446bbf77bea06f23bba8155e
yasui1992 <yasui1992github@gmail.com>
2025-06-13 06:17:35 +0900

    Add duckdb extentions

---
+++ mnt/notebooks/duckdb.py
@@ -0,0 +1,37 @@
+import marimo
+
+__generated_with = "0.13.15"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    _df = mo.sql(
+        f"""
+        CREATE OR REPLACE TABLE penguins
+        AS
+        SELECT * FROM read_json("https://github.com/vega/vega-datasets/raw/main/data/penguins.json")
+        ;
+        """
+    )
+    return (penguins,)
+
+
+@app.cell
+def _(mo, penguins):
+    _df = mo.sql(
+        f"""
+        SELECT * FROM penguins;
+        """
+    )
+    return
+
+
+if __name__ == "__main__":
+    app.run()
